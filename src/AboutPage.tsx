4dd526c41b5d3d947cb1bd4823696ba30d14af5c
marc <schweringmarc01@gmail.com>
2021-04-08 11:53:40 +0200

    added gql lambda text

--- src/AboutPage.tsx
+++ src/AboutPage.tsx
@@ -1,85 +1,138 @@
 import React from "react";
 import { Box, Heading, Paragraph, Anchor } from "grommet";
 import AppBar from "./AppBar";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-const windyLink = (
-  <Anchor href="https://www.windy.com/" label="windy.com" target="_blank" />
-);
-
-const repoLink = (
+const gqlPlaygroundLink = (
   <Anchor
-    href="https://github.com/mRcSchwering/prevailing-winds"
-    label="github.com"
+    href="https://github.com/prisma/graphql-playground"
+    label="graphql playground (github.com)"
+    target="_blank"
+  />
+);
+const ariadneLink = (
+  <Anchor href="https://ariadnegraphql.org/" label="Ariadne" target="_blank" />
+);
+const uvicornLink = (
+  <Anchor href="https://www.uvicorn.org/" label="Uvicorn" target="_blank" />
+);
+const backendLink = (
+  <Anchor
+    href="https://github.com/mRcSchwering/prevailing-winds/tree/main/backend"
+    label="prevailing-winds backend (github.com)"
     target="_blank"
   />
 );
 
-const copernicusLink = (
-  <Anchor
-    href="https://cds.climate.copernicus.eu/"
-    label="cds.climate.copernicus.eu"
-    target="_blank"
-  />
-);
+const yamlDef = `
+Resources:
+  GqlPostFunction:
+    Type: AWS::Serverless::Function
+    Properties:
+      CodeUri: src/
+      Handler: graphql_post.lambda_handler
+      Runtime: python3.8
+      Policies:
+        - S3ReadPolicy:
+            BucketName: !Ref ContentBucket
+      Events:
+        Gql:
+          Type: Api
+          Properties:
+            Path: /graphql
+            Method: post
+            RestApiId: !Ref ApiDeployment`;
 
-const gdprLink = (
-  <Anchor href="https://gdpr.eu/cookies/" label="gdpr.eu" target="_blank" />
-);
+const starletteGql = `
+from pathlib import Path
+from ariadne import load_schema_from_path, make_executable_schema
+from ariadne.asgi import GraphQL
+from src.queries import queries
 
-const era5Link = (
-  <Anchor
-    href="https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-single-levels?tab=overview"
-    label="ERA5 hourly data on single levels from 1979 to present (cds.climate.copernicus.eu)"
-    target="_blank"
-  />
-);
+this_dir = Path(__file__).parent.absolute()
+schema_def = load_schema_from_path(str(this_dir))
+schema = make_executable_schema(schema_def, *queries)
 
-const dataprepLink = (
-  <Anchor
-    href="https://github.com/mRcSchwering/prevailing-winds/blob/main/prep/2_calc_avg_winds.py"
-    label="prep/2_calc_avg_winds.py (github.com)"
-    target="_blank"
-  />
-);
+app = GraphQL(schema)`;
+
+const lambdaHandler = `
+from ariadne import graphql_sync
+from src.handler import Event, Context, form_output
+from src.schema import schema
+
+def lambda_handler(event_dict: dict, _: Context):
+    event = Event(**event_dict)
+    success, result = graphql_sync(
+        schema=schema, data=event.body, context_value={"request": event}
+    )
+    return form_output(status=200 if success else 400, body=result)`;
 
 export default function AboutPage(): JSX.Element {
   return (
     <Box fill>
       <AppBar />
       <Box flex align="center" pad="medium" overflow={{ horizontal: "hidden" }}>
-        <Heading level="3">About</Heading>
+        <Heading level="3">Setup Endpoint in template.yaml</Heading>
         <Paragraph>
-          There are many great apps like {windyLink} that show you the current
-          winds (and more) with short-term predictions. However, I didn't really
-          find anything with historic wind data for long-term planning,
-          <i> e.g.</i> when planning a Törn or passage. So I made this app. The
-          code is all on {repoLink}. The data is derived from {copernicusLink}{" "}
-          datasets.
+          You only need one endpoint to answer POST requests. So, the SAM
+          `template.yaml` definition can be as little as the code below. I
+          define the endpoint with lambda function that handles POSTs and I put
+          it on <code> /graphql </code>
+          since this where most frameworks expect this endpoint to be. In my
+          case the lambda function needs read access to an S3 bucket as well,
+          which I provide with a policy.
         </Paragraph>
+        <SyntaxHighlighter language="yaml" style={docco}>
+          {yamlDef}
+        </SyntaxHighlighter>
         <Paragraph>
-          You might have noticed there was no <b>cookie banner</b> you had to
-          click away ({gdprLink}). This is because I don't track any information
-          about you and don't give anything to third-party vendors.
+          What you can do on top of that is to add a GET endpoint on{" "}
+          <code> / </code>
+          as well. This is not strictly necessary for your API to work, but it
+          makes it easier to develop. You can serve {gqlPlaygroundLink}
+          there which gives you a tool play with your API and develop queries.
         </Paragraph>
-        <Heading level="4">Data</Heading>
+        <Heading level="3">Ariadne for Python GraphQL</Heading>
         <Paragraph>
-          I used {era5Link} as a basis for calculating wind strengths and
-          directions. This dataset is a ECMWF reanalysis which holds hourly data
-          of a lot of variables like wind, pressure, temperature. Winds were
-          regridded to a regular lat-lon grid of 0.25 degrees. To reduce the
-          download sizes I only took every 3rd hour (00:00, 03:00, 06:00, ...)
-          and exluded 20° from the poles (70°S to 70°N).
+          I'm using {ariadneLink} to develop the API in python. You first define
+          a schema (usually as a <code> .graphql </code>) file and then write
+          resolvers for it. The resolvers are just normal python functions. The
+          code below shows a simple app. Here, <code> queries </code> is a list
+          that contains all the resolver functions, and{" "}
+          <code> schema_def </code> is the schema definition from the{" "}
+          <code> .graohql </code> file. Both of them are combined into an ASGI
+          app which you could serve with {uvicornLink}.
         </Paragraph>
+        <SyntaxHighlighter language="python" style={docco}>
+          {starletteGql}
+        </SyntaxHighlighter>
+        <Heading level="3">Ariadne in Lambda</Heading>
         <Paragraph>
-          The data is prepared by averaging the wind vectors over certain areas.
-          I chose to average over the area of 1°N and 1°E at every full degree
-          lat-lng. With the lat-lon regridded data points every 0.25 degree this
-          is an average over 16 data points at each full degree lat-lng. At the
-          equator this area spans 3,600M<sup>2</sup>. Going further to either
-          pole this area gets smaller. Then, I used the Beaufort scale for
-          binning wind velocities and the 16 traditional compass rose bearings
-          (N, NNE, NE, ...) for binning wind directions. If you are interested
-          in the details, this data preparation is done in {dataprepLink}.
+          Ariadne also has a <code> graphql_sync </code> function which lets you
+          execute a single query synchronously (without the ASGI app). This is
+          what I use in the lambda handler of the <code> /graphql </code> POST
+          endpoint.
+          <code> Event, Context, form_output </code> are just helpers to make
+          the request and response dictionaries a little easier to handle. For
+          details see the backend code of one webapp {backendLink}.
+        </Paragraph>
+        <SyntaxHighlighter language="python" style={docco}>
+          {lambdaHandler}
+        </SyntaxHighlighter>
+        <Paragraph>
+          Now you might wonder how inefficient this setup is. On every request,
+          the schema definition and resolvers are initialized before the actual
+          query is handled. That's true. In fact, this setup is about 10x to
+          100x slower than having the Ariadne app running on a EC2 instance.
+          Nevertheless, it is fast enough for me. I found this setup to have an
+          overhead of roughly 150ms (on top of the actual query execution) in a
+          hot lambda function. A nice side-effect of this setup is, that it is
+          less likely for the Lambda function to be cold, since all requests
+          trigger the same lambda function. <i>E.g.</i>a frontend could trigger
+          a request on initialization, activating a possibly cold Lambda
+          function. Further, user-triggered requests would then be answered by
+          an already hot Lambda function.
         </Paragraph>
       </Box>
     </Box>
