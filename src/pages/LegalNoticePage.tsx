7d9bf71a978a8c0500bf0326c845f13cd9fdb0b7
marc <schweringmarc01@gmail.com>
2021-07-18 20:18:09 +0200

    added legal notice template

--- src/pages/LegalNoticePage.tsx
+++ src/pages/LegalNoticePage.tsx
@@ -4,21 +4,90 @@ import { AppHeader, Section, AppFooter } from "../components";
 
 export default function LegalNoticePage(): JSX.Element {
   return (
-    <Box style={{ height: "100vh" }}>
+    <Box>
       <AppHeader />
-      <Section style={{ marginBottom: "30vh" }}>
+      <Section>
         <Heading level="2">Legal Notice</Heading>
-        <Paragraph textAlign="center">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et
-          faucibus dui. Interdum et malesuada fames ac ante ipsum primis in
-          faucibus. Vivamus vel fringilla ex, in interdum ex. Morbi eu turpis eu
-          sem interdum finibus sit amet non ante. Aenean dictum egestas
-          molestie. Vivamus molestie, lectus vitae condimentum consequat, massa
-          dolor malesuada eros, convallis euismod est velit at ex. Morbi eu
-          maximus mauris. Nulla nec ipsum quis erat auctor viverra. Donec eget
-          convallis felis. Phasellus cursus orci eget quam tempor iaculis. Fusce
-          posuere arcu sed dolor accumsan porttitor. Aenean eu mauris non turpis
-          condimentum malesuada. Fusce venenatis convallis neque nec eleifend.
+        <Paragraph>
+          <b>Marc Schwering</b>
+          <br />
+          xxxxxxxxxxxxxxxxxxxxxxxxx xxx
+          <br />
+          xxxxxxx Berlin
+          <br />
+          <br />
+          <b>Contact</b>
+          <br />
+          Telephone: xxxxxxxxxxxxxxx
+          <br />
+          E-mail: info@vordeck.de
+          <br />
+          <br />
+          Value added tax identification number: xxxxxxxxxxxxxxx
+          <br />
+          Job title: xxxxxxxxxxxxxxxx
+          <br />
+          Awarded in: Germany
+          <br />
+          <br />
+          <b>Verbraucher­streit­beilegung/Universal­schlichtungs­stelle</b>
+          <br />
+          Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren
+          vor einer Verbraucherschlichtungsstelle teilzunehmen.
+          <br />
+          <br />
+          <b>Haftung für Inhalte</b>
+          <br />
+          Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte
+          auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach
+          §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht
+          verpflichtet, übermittelte oder gespeicherte fremde Informationen zu
+          überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
+          Tätigkeit hinweisen.
+          <br />
+          <br />
+          Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
+          Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
+          Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der
+          Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden
+          von entsprechenden Rechtsverletzungen werden wir diese Inhalte
+          umgehend entfernen.
+          <br />
+          <br />
+          <b>Haftung für Links</b>
+          <br />
+          Unser Angebot enthält Links zu externen Websites Dritter, auf deren
+          Inhalte wir keinen Einfluss haben. Deshalb können wir für diese
+          fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
+          verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der
+          Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der
+          Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige
+          Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.
+          <br />
+          <br />
+          Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch
+          ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
+          Bekanntwerden von Rechtsverletzungen werden wir derartige Links
+          umgehend entfernen.
+          <br />
+          <br />
+          <b>Urheberrecht</b>
+          <br />
+          Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
+          Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung,
+          Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
+          Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des
+          jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite
+          sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+          <br />
+          <br />
+          Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt
+          wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden
+          Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf
+          eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+          entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen
+          werden wir derartige Inhalte umgehend entfernen.
+          <br />
         </Paragraph>
       </Section>
       <AppFooter />
