73981c12f429b17fe1a32d19b177a3a4478117e1
marc <schweringmarc01@gmail.com>
2021-10-19 21:30:09 +0200

    finished writing api gateway + cloudflare KN

--- src/kns/ApiGatewayCloudflareDeploy.tsx
+++ src/kns/ApiGatewayCloudflareDeploy.tsx
@@ -1,16 +1,29 @@
 import { P, H4, Link, Code, BlockCode, Ol, Dli } from "./components";
 
-const allowPublicPolicy = `{
-  "Version": "2012-10-17",
-  "Statement": [
-      {
-          "Effect": "Allow",
-          "Principal": "*",
-          "Action": "s3:GetObject",
-          "Resource": "arn:aws:s3:::mywebsite-frontend/*"
-      }
-  ]
-}`;
+const templateYaml = `Resources:
+
+  GqlPostFunction:
+    Type: AWS::Serverless::Function
+    Properties:
+      CodeUri: src/
+      Handler: graphql_post.lambda_handler
+      Runtime: python3.8
+      Events:
+        Gql:
+          Type: Api
+          Properties:
+            Path: /graphql
+            Method: post
+            RestApiId: !Ref ApiDeployment
+
+  ApiDeployment:
+    Type: AWS::Serverless::Api
+    Properties:
+      StageName: main
+      Domain:
+        DomainName: backend.mywebsite.de
+        CertificateArn: arn:aws:acm:us-east-1:somehash
+        EndpointConfiguration: EDGE`;
 
 export default function Page(): JSX.Element {
   return (
@@ -24,21 +37,16 @@ export default function Page(): JSX.Element {
         make a great combination. It basically allows you to deploy a serverless
         webapp (with backend and database) for free (almost). Here, I describe
         how to set up a backend using{" "}
-        <Link href="https://www.cloudflare.com/" label="Cloudflare" /> and
+        <Link href="https://www.cloudflare.com/" label="Cloudflare" /> and{" "}
         <Link
           href="https://aws.amazon.com/api-gateway/"
           label="AWS API Gateway"
         />
-        . Here, the backend is a GraphQL API served by a Lambda function behind
-        the API Gateway. For describing the infrastructure on AWS I am using the{" "}
+        . The backend is a GraphQL API served by a Lambda function behind the
+        API Gateway. For describing the infrastructure on AWS I am using the{" "}
         <Link
           href="https://aws.amazon.com/serverless/sam/"
           label="SAM framework"
-        />
-        . The setup could be something like{" "}
-        <Link
-          label="described here"
-          href="https://vordeck.de/kn/lambda-graphql"
         />
         .
       </P>
@@ -46,129 +54,93 @@ export default function Page(): JSX.Element {
       <P>
         You need a domain. Let's say you own <b>mywebsite.de</b>. Additionally,
         you need a{" "}
-        <Link href="https://www.cloudflare.com/" label="Cloudflare" /> (free
-        tier) and an <Link href="https://aws.amazon.com/" label="AWS" />{" "}
-        account. In order to use your domain with Cloudflare, you need to
-        transfer it from your registrar to Cloudflare.{" "}
+        <Link href="https://www.cloudflare.com/" label="Cloudflare" /> and an{" "}
+        <Link href="https://aws.amazon.com/" label="AWS" /> account. In order to
+        use your domain with Cloudflare, you need to transfer it from your
+        registrar to Cloudflare.{" "}
         <Link
           href="https://developers.cloudflare.com/registrar/get-started/transfer-domain-to-cloudflare"
           label="Here"
         />{" "}
-        are some instructions on how to do that. Finally, you need an SPA / some
-        static files you want to host as a website.
+        are some instructions on how to do that.
       </P>
-      <H4>S3 Bucket</H4>
-      <Ol>
-        <Dli label="Create S3 bucket">
-          In the AWS console, open S3 and create a bucket. You could name it{" "}
-          <i>mywebsite-frontend</i> for example
-        </Dli>
-        <Dli label="Upload SPA">
-          You can upload all files using the AWS console.
-        </Dli>
-        <Dli label="Allow public access">
-          go to <i>Permissions</i> and uncheck all public access blocks. Then
-          edit the bucket policy like shown below.
-        </Dli>
-        <Dli label="Whait for global S3 URL to become active">
-          There is a local URL of your bucket (like{" "}
-          <i>http://s3-eu-central-1.amazonaws.com/mywebsite-frontend/</i>) and a
-          global (like <i>http://s3.amazonaws.com/mywebsite-frontend/</i>). The
-          first one is immediately active upon bucket creation. The second one
-          can take up to a few hours. Cloudfront (later) is easier to set up if
-          the global version is already available.
-        </Dli>
-      </Ol>
+      <H4>Create a certificate</H4>
       <P>
-        With the policy below everyone is able to download all objects within
-        your bucket. Replace the name <Code>mywebsite-frontend</Code> with the
-        name of your S3 bucket.
-      </P>
-      <BlockCode code={allowPublicPolicy} lang="json" />
-      <H4>Certificate Manager</H4>
-      <P>
-        In AWS console open Certificate Manager and create a{" "}
-        <b>custom AWS-managed certificate</b>. As domains add your domain as
-        CNAME. It's convenient to also add a CNAME for all subdomains.
-        <i>E.g.</i> <Code>mywebsite.de</Code> and <Code>*.mywebsite.de</Code>.
-      </P>
-      <H4>Cloudfront Distribution</H4>
-      <Ol>
-        <Dli label="Create a Cloudfront distribution">
-          In AWS console open Cloudfront and create a new distribution.
-        </Dli>
-        <Dli label="Chose S3 bucket as origin">
-          Chose the S3 bucket you just created as origin. Use its global URL.
-          You should be able to select it from the dropdown.
-        </Dli>
-        <Dli label="Set a custom SSL certificate">
-          Set the custom AWS-managed SSL certificate for your domain that you
-          created above.
-        </Dli>
-        <Dli label="(Set custom error for redirect)">
-          If you have different routes in your SPA you will need to redirect
-          every request to <i>index.html</i>. For that go to <i>Error Pages</i>{" "}
-          and create a custom error response with error Code 403, minimum TTL 0,
-          response path <i>index.html</i>, and response Code 200.
-        </Dli>
-        <Dli label="Get distribution domain name">
-          Deploy the distribution and get a distribution domain name (some URL
-          that looks like this <i>https://d1dyasvndfdski.cloudfront.net</i>).
-        </Dli>
-      </Ol>
-      <H4>Cloudflare Setup</H4>
-      <Ol>
-        <Dli label="Select your domain">
-          After your domain was transferred to Cloudflare you should be able to
-          select it in their console.
-        </Dli>
-        <Dli label="Set Full (strict) SSL/TLS encryption">
-          Select <i>SSL/TLS</i> then select <i>Full (strict)</i>.
-        </Dli>
-        <Dli label="Edit DNS record">
-          Select <i>DNS</i>, then select the record named <i>mywebsite.de</i>.
-          If it's not there create a new record. Make it a <i>CNAME</i> record.
-          As content enter the Cloudfront distribution domain name from above.
-          The proxy status should be <i>Proxied</i>
-        </Dli>
-        <Dli label="(Add www CNAME record)">
-          Optionally add another <i>CNAME</i> with name <i>www</i> and the
-          Cloudfront distribution domain name as content.
-        </Dli>
-      </Ol>
-      <P>
-        Afterwards the SPA should be reachable from <i>https://mywebsite.de</i>{" "}
-        and <i>https://www.mywebsite.de</i>. If not try to access the different
-        URLs to find out where the problem is. If you can't reach the S3 URL (
-        <i>http://s3.amazonaws.com/mywebsite-frontend/</i>) the URL might not be
-        active yet or the permissions are wrong.
-      </P>
-      <H4>Without Cloudfront</H4>
-      <P>
-        There is a way of setting this up without Cloudfront. This usually
-        involves using the <i>website hosting</i> feature of S3 and then setting
-        Cloudflare's SSL/TLS to <i>flexible</i>. In this case the S3 bucket
-        needs to bear the domain name of your website (so the S3 bucket must be
-        named <i>mywebsite.de</i> for example). There are many tutorials for
-        this setup (
+        We need to create a custom AWS-managed certificate for your domain. In
+        AWS this is done in the AWS certificate manager. This must be done in{" "}
+        <b>us-east-1</b> (
         <Link
-          href="https://medium.com/@hranicka/hosting-a-static-website-amazon-s3-cloudflare-127b57a13461"
-          label="here is one"
+          label="AWS dev guide"
+          href="https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/cnames-and-https-requirements.html"
         />
-        ). The main problem with this setup is that you need to set Cloudfront's
-        SSL/TLS to <i>flexible</i>. And unfortunately in Cloudfront you can only
-        set the whole domain to one SSL/TLS setting. So, if any other resource
-        under your domain is already encrypted, you cannot serve define it in
-        Cloudflare anymore. Say your backend is an AWS API Gateway (which comes
-        already with <i>https</i>). You will have to use its direct URL (which
-        looks something like{" "}
-        <i>
-          https://tpdfuasbh6.execute-api.eu-central-1.amazonaws.com/stage/graphql/
-        </i>
-        ). By using Cloudfront S3 is encrypted as well and Cloudflare can be set
-        to <i>Full (strict)</i>, thus allowing you to hide everything behind
-        Cloudflare.
+        ). Even if the rest of your infrastructure is in say <i>eu-central-1</i>
+        . The certificate should be in <i>us-east-1</i>.
       </P>
+      <Ol>
+        <Dli label="Request certificate">
+          In the AWS console open AWS Certificate Manager in <i>us-east-1</i>{" "}
+          and click on <i>Request certificate</i>. Request a public certificate.
+        </Dli>
+        <Dli label="Define domain name">
+          Add your domain name like <Code>mywebsite.de</Code>. Also add a name
+          for subdomains like <Code>*.mywebsite.de</Code>
+        </Dli>
+        <Dli label="Validate certificate">
+          I would recommend DNS validation. AWS will give you a few TXT records
+          that you have to enter in your domain's DNS records on Cloudflare. It
+          can take a few minutes for AWS to recognize the records.
+        </Dli>
+      </Ol>
+      <H4>Deploy backend with custom domain</H4>
+      <P>
+        Say you have a setup{" "}
+        <Link label="like this" href="https://vordeck.de/kn/lambda-graphql" />:
+        Your backend is in Lambda functions behind an API Gateway, everything is
+        orchestrated using SAM.
+      </P>
+      <Ol>
+        <Dli label="Define API Deployment Resource">
+          As shown in the code snippet below add an <i>ApiDeployment</i>{" "}
+          resource in your <i>template.yaml</i>. The <i>DomainName</i> must be a
+          subdomain of your domain. As <i>CertificateArn</i> enter the ARN of
+          the custom AWS-managed certificate created earlier.
+        </Dli>
+        <Dli label="Use API Deployment">
+          In <i>template.yaml</i> in the definition of your endpoint, reference
+          the API deployment as a <i>RestApiId</i> property.
+        </Dli>
+        <Dli label="Deploy SAM">
+          Now run <Code>sam deploy</Code> and wait for the deployment to finish.
+        </Dli>
+        <Dli label="Get API Gateway domain name">
+          In the AWS console open <i>API Gateway</i> and click on{" "}
+          <i>Custom Domain Names</i>. Your backend domain name should be listed
+          there. Click on it. In the endpoint configuration window take the{" "}
+          <i>API Gateway domain name</i>. It should look somthing like{" "}
+          <Code>dr341df1nraso.cloudfront.net</Code>.
+        </Dli>
+      </Ol>
+      <BlockCode code={templateYaml} lang="yaml" />
+      <H4>Cloudflare Setup</H4>
+      <P>
+        Now we just need to add a DNS record in Cloudflare. Login to Cloudflare
+        and select your domain.
+      </P>
+      <Ol>
+        <Dli label="Check Full (strict) SSL/TLS encryption">
+          Under <i>SSL/TLS</i> encryption should be set to <i>Full (strict)</i>.
+        </Dli>
+        <Dli label="Add CNAME">
+          Select <i>DNS</i>, then add a new CNAME record. Give it the name of
+          your subdomain <i>e.g. backend</i>. As value use the API Gateway
+          domain name from earler.
+        </Dli>
+        <Dli label="Test">
+          After saving wait a minute then try to reach your backend via
+          Cloudflare. In this example that would be a POST to{" "}
+          <i> https://backend.mywebsite.de/graphql</i>.
+        </Dli>
+      </Ol>
     </>
   );
 }
