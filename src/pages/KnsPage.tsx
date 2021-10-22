6e0e40d5457a5e94fdabce43434eae0c919de89d
marc <schweringmarc01@gmail.com>
2021-10-22 15:57:43 +0200

    Revert "WIP only js allowed for sitemap generator"
    This reverts commit d4d3412bcb2c80778c4a4032bcb37247d6d0619c.

--- src/pages/KnsPage.tsx
+++ src/pages/KnsPage.tsx
@@ -12,7 +12,7 @@ import {
 import { Search } from "grommet-icons";
 import { useHistory } from "react-router-dom";
 import { AppHeader, AppFooter } from "../components";
-import registry from "../kns/registry";
+import { registry } from "../kns";
 
 type FilterProps = {
   search: string;
