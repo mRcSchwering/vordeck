fecae7a76865cb23c0250ef915cfa892e150d9ca
Marc Schwering <schweringmarc01@gmail.com>
2022-10-13 19:51:18 +0200

    yaml evnars

--- src/kns/YamlEnvars.tsx
+++ src/kns/YamlEnvars.tsx
@@ -1,4 +1,4 @@
-import { P, Link, BlockCode, Img } from "./components";
+import { P, BlockCode, Img, Code } from "./components";
 
 const funDef = `#!/usr/bin/env bash
 # generate a .yml file from one of the *tmplt.yml files
@@ -8,7 +8,7 @@ const funDef = `#!/usr/bin/env bash
 #
 # Export all envars, then use with tmplt yml path:
 #
-#   bash k8s/get_env_yml.sh k8s/stage_values.tmlpt.yaml
+#   bash k8s/get_env_yml.sh k8s/stage_values.tmplt.yaml
 #
 echo "Generating $1..."
 ymlfile="\${1/.tmplt/}"
@@ -40,24 +40,6 @@ done
 rm -f "$ymlfile.bak"
 `;
 
-const example = `>>> d = {"a": {"b": [1, {"c": "asd"}]}}
->>> find_key_in_json("c", d)
-['a', 'b', 1, 'c']`;
-
-const tests = `@pytest.mark.parametrize(
-  "obj, exp",
-  [
-      ({"b": 1, "a": {"x": -1}}, ["a", "x"]),
-      ({"a": {"b": {"x": -1}}}, ["a", "b", "x"]),
-      ({"a": [0, 0, {"x": -1}]}, ["a", 2, "x"]),
-      ([0, {"x": -1}], [1, "x"]),
-  ],
-)
-def test_find_key_in_json(obj, exp):
-  res = find_key_in_json(key="x", parsed_json=obj)
-  assert res == exp
-`;
-
 export default function Page(): JSX.Element {
   return (
     <>
@@ -67,28 +49,26 @@ export default function Page(): JSX.Element {
         height="250px"
       />
       <P>
-        The amount of time I spend looking through JSON objects is ridiculous.
-        Usually it's some API response with different levels of metadata, nodes,
-        cursors and so on... and actually you just need one value. First of all,
-        there are gists like{" "}
-        <Link label="this one" href="https://gist.github.com/iwek/3924925" />.
-        But usually I am in python and just need one particular key. Thus, I
-        have this snippet:
+        It has become quite common to declare app configurations in YAML files.
+        Sooner or later I usually end up having several versions of the same
+        YAML file which each describe a slightly different environment,{" "}
+        <i>e.g.</i> develop and production. This can be nicely solved by using
+        environment variables. However, there is no standard for defining
+        environment variables in YAML. Some technologies like docker, gitlab-ci,
+        k8s already have mechanisms for handling environment variables in place.
+        But if you are not using any of these, you need to come up with your own
+        mechanism. Here, is mine:
       </P>
-      <BlockCode code={funDef} lang="bash" />
+      <BlockCode code={funDef} lang="bash" label="get_env_yml.sh" />
       <P>
-        It's a breadth-first-search that returns the first occurence of that key
-        as a list of keys and list indices that lead to this key. Like this:
+        You write a YAML template (say <Code>config.tmplt.yaml</Code>) as usual
+        using the bash syntax for resolving environment variables (
+        <Code>{"${my_envar}"}</Code>). Then you export your environment
+        variables and use the above script to generate the proper YAML file (
+        <Code>bash get_env_yml.sh config.tmplt.yaml</Code>). In the resulting{" "}
+        <Code>config.tmplt.yaml</Code> the environment variables were resolved.
+        This way you can use the same YAML file for different configurations.
       </P>
-      <BlockCode code={example} lang="python" />
-      <P>
-        Note, this is for JSON. You might think you can use this for dicts in
-        general... don't. Whereas in JSON object keys are strings and list
-        indices are integers, a plain python dict can contain non-string keys,
-        tuples, and any object basically. Finally, for all the people as
-        paranoid as me, here are some tests for this function.
-      </P>
-      <BlockCode code={tests} lang="python" />
     </>
   );
 }
