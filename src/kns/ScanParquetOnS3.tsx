021728a8055e67598e2d253fd2360737dc38f6af
Marc Schwering <schweringmarc01@gmail.com>
2024-10-13 20:17:26 +0200

    typos

--- src/kns/ScanParquetOnS3.tsx
+++ src/kns/ScanParquetOnS3.tsx
@@ -107,13 +107,9 @@ export default function Page(): JSX.Element {
         I wanted to see how practial it is to query{" "}
         <A href="https://parquet.apache.org/" label="Parquet" /> files on S3
         directly using <A href="https://docs.pola.rs/" label="Polars" /> and{" "}
-        <A href="https://arrow.apache.org/" label="PyArrow" />.{" "}
-        <b>Here's my tl;dr:</b>
-        Files are basically downloaded, then queried. I assumed pyarrow would
-        somehow be able to pushdown predicates over S3. But it doesn't. Of
-        course, in a partitioned dataset, filtering to only a few partitions,
-        will reduce the download size. But filtering within a parquet file will
-        happen on the local machine.
+        <A href="https://arrow.apache.org/" label="PyArrow" />. <b>tl;dr: </b>
+        Predicate pushdown stops on the file level. The smallest element is a
+        whole parquet file.
       </P>
       <Heading variant="h4">Test data</Heading>
       <P>
@@ -122,14 +118,14 @@ export default function Page(): JSX.Element {
           href="https://developer.imdb.com/non-commercial-datasets/"
           label="imdb"
         />
-        : <i>title.ratings.tsv</i> with 1.5 Mio rows of movie ratings and{" "}
-        <i>title.basics.tsv</i> with 11 Mio rows of basic information about
-        movies. One can be joined to the other over a variable{" "}
-        <Code>tconst</Code>. The code below creates a parquet file for{" "}
-        <i>ratings</i> and a hive-partitioned parquet dataset for <i>basics</i>{" "}
-        with <Code>startYear</Code> as partitioning variable. It is recommended
-        to create fewer than 10k partitions (ok) with files not larger than 2GB
-        (ok) and not smaller than 20Mb (not ok). Most files in the partitioned
+        : <i>title.ratings.tsv</i> with 1.5M rows of movie ratings and{" "}
+        <i>title.basics.tsv</i> with 11M rows of basic information about movies.
+        One can be joined to the other over a variable <Code>tconst</Code>. The
+        code below creates a parquet file for <i>ratings</i> and a
+        hive-partitioned parquet dataset for <i>basics</i> with{" "}
+        <Code>startYear</Code> as partitioning variable. It is recommended to
+        create fewer than 10k partitions (ok) with files not larger than 2GB
+        (ok) and not smaller than 20Mb (soso). Most files in the partitioned
         dataset are smaller than 20Mb.
       </P>
       <BlockCode code={dataPrep} lang="python" label="data_prep.py" />
@@ -143,37 +139,37 @@ export default function Page(): JSX.Element {
       <BlockCode
         code={query1}
         lang="python"
-        label="scan 1.5Mio rows, result 3k rows: 2s"
+        label="scan 1.5M rows, result 3k rows: 2s"
       />
       <P>
         I think I secretly hoped that predicate pushdown, which works well with
         local parquet files, would also somehow work with S3 parquet files. The
         filter above with average rating and number of votes selects about 3k
-        rows. But it doesn't really matter here. Without filters the query will
-        also take 2s.
+        rows. But it doesn't really matter here. Without filters the query also
+        takes 2s.
       </P>
       <P>
-        It becomes painful with larger datasets. Scanning the <i>basics</i>{" "}
-        dataset with 11Mio rows on S3 takes about 110s. It is actually faster to
-        use boto to download the files into a temporary directory and then run
-        the query locally.
+        It becomes painful with larger datasets. Scanning{" "}
+        <i>title.basics.tsv</i> with 11Mio rows on S3 takes about 110s. It is
+        actually faster to use boto to download the files into a temporary
+        directory and then run the query locally.
       </P>
       <BlockCode
         code={query2}
         lang="python"
-        label="scan 11Mio rows, use 3k rows as filter: 110s"
+        label="scan 11M rows, use 3k rows as filter: 110s"
       />
       <P>
         A filter on partitions is recognized fortunately. Below is the same
         query but with an additional filter for a movies of a specific{" "}
-        <Code>startYear</Code>.<Code>startYear</Code> was a partition of the
-        dataset. So now only one parquet file is downloaded and queried. The
-        whole query now "only" took 16s.
+        <Code>startYear</Code>. <Code>startYear</Code> was a partition of the
+        dataset. With that one parquet file is downloaded and queried. The whole
+        query now "only" took 16s.
       </P>
       <BlockCode
         code={query4}
         lang="python"
-        label="scan 11Mio rows, single partition and 3k rows as filter: 16s"
+        label="scan 11M rows, single partition and 3k rows as filter: 16s"
       />
     </>
   );
